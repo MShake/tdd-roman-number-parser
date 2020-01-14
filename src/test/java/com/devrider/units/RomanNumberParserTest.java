91be7489e6b2046e46ab51fc1cc6e0fa8f4afdf3
Antoine Fidalgo <fidalgo.antoine@gmail.com>
2020-01-14 21:43:42 +0100

    [FLG] clean useless import

--- src/test/java/com/devrider/units/RomanNumberParserTest.java
+++ src/test/java/com/devrider/units/RomanNumberParserTest.java
@@ -7,7 +7,6 @@ import com.devrider.RomanNumberOutOfRangeException;
 import com.devrider.RomanNumberParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
